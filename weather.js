c817883d858b229431f48f3dc41a7a9df71fee89
jominji <whalswl576@gmail.com>
2024-08-07 01:44:11 +0900

    end weather / next weather img fixed

--- weather.js
+++ weather.js
@@ -1,18 +1,13 @@
 
-// require('dotenv').config();
 import config from './config.js';
   
   //  API_KEY 
 const  API_KEY = config.WEATHER_API_KEY;
-const ex_url =`http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=${API_KEY}&numOfRows=10&pageNo=1&base_date=20210628&base_time=0600&nx=55&ny=127`;
-  console.log(ex_url);
+
 
 //  초단기 예보조회 :    http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst
 //  단기예보 하루치를 다 알려주는듯합니다 :  http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst
 
-
-// let year = today.getFullYear(); // 년도
-// let day = today.getDay();  // 요일
 
 
 // const nx = '60';
@@ -21,25 +16,6 @@ const nx = '68';
 const ny = '120';
 
 
-// 요게 오늘 날짜
-// let today = new Date();  
-// let year = today.getFullYear(); // 년도
-// let month = ("0"+( today.getMonth() + 1) ).slice(-2);  // 월
-// let date = today.getDate();  // 날짜
-// const baseDate=1;
-// console.log(year+month+date);
-
-
-
-// let d = new Date();
-// let sel_day = -1; //일자를 조절하시면 됩니다. -1이면 하루전/ +1이면 내일
-// d.setDate(d.getDate() + sel_day );
-
-
-// let year    = d.getFullYear();
-// let month   = ('0' + (d.getMonth() +  1 )).slice(-2);
-// let day     = ('0' + d.getDate()).slice(-2);
-// dt = year+"-"+month+"-"+day;
 
 
 let today = new Date();  
@@ -95,7 +71,6 @@ nextHours_4 = ("0" + nextHours_4).slice(-2)+"00";
 nextHours_5 = ("0" + nextHours_5).slice(-2)+"00";
 
 
-console.log(nextHours_1, nextHours_2, nextHours_3,nextHours_4, nextHours_5);
 
 
 const getWeatherApi = (getWeatherList) => {
@@ -158,9 +133,25 @@ const nextDayWeatherImage5 = document.querySelector('.next--weather--5 span:nth-
 const nextDayWeatherHours5 = document.querySelector('.next--weather--5 span:last-child');
 
 
+const minTempSpan = document.querySelector('.weather--temper span:first-child');
+const maxTempSpan = document.querySelector('.weather--temper span:last-child');
+
+
+
 const getWeatherList = (weatherList) =>{
     
    
+    const tempArray = [];
+
+    weatherList.forEach( item =>{
+        if (item.category === "TMP") {
+            tempArray.push(item.fcstValue);
+        }
+    });
+    const todayMaxTemp = Math.max(...tempArray);
+    const todayMinTemp = Math.min(...tempArray);
+    minTempSpan.textContent = `${todayMinTemp}°`;
+    maxTempSpan.textContent = `${todayMaxTemp}°`;
 
 
     function getValuesForFcstTime(fcstTime) {
@@ -169,6 +160,7 @@ const getWeatherList = (weatherList) =>{
         let ptyValue = null;
 
         weatherList.forEach(item => {
+
             if (item.fcstTime === fcstTime) {
                 if (item.category === "TMP") {
                     tmpValue = item.fcstValue;
@@ -186,9 +178,9 @@ const getWeatherList = (weatherList) =>{
 
     const values = getValuesForFcstTime(nowFcstTime);
 
-    console.log(`TMP value at ${nowFcstTime}: ${values.tmpValue}`);
-    console.log(`SKY value at ${nowFcstTime}: ${values.skyValue}`);
-    console.log(`PTY value at ${nowFcstTime}: ${values.ptyValue}`);
+    // console.log(`TMP value at ${nowFcstTime}: ${values.tmpValue}`);
+    // console.log(`SKY value at ${nowFcstTime}: ${values.skyValue}`);
+    // console.log(`PTY value at ${nowFcstTime}: ${values.ptyValue}`);
 
     const nextWeather1 = getValuesForFcstTime(nextHours_1);
     const nextWeather2 = getValuesForFcstTime(nextHours_2);
@@ -206,93 +198,6 @@ const getWeatherList = (weatherList) =>{
     addClassListWeatherIcon(nextHours_5, nextWeather5, nextDayWeatherImage5);
 
 
-
-
-
-    // if(Number(values.skyValue) === 1){
-    //     todayWeatherSecondSpan.textContent = '맑음';
-
-    //     if(  19 < nowFcstTime.slice(0,-2) ) { // 20시 부터 저녁 icon을 보여주기 위함
-    //         if( Number(values.ptyValue) === 0 ){
-    //             todayWeatherLogo.classList.add('weather-icon--밤--맑음');
-    //         }else if( Number(values.ptyValue) === 1){ // 비
-    //             todayWeatherLogo.classList.add('weather-icon--밤--비--맑음');
-    //         }else if( Number(values.ptyValue) === 2){ //눈/비
-    //             todayWeatherLogo.classList.add('weather-icon--밤--눈비--맑음');
-    //         }else if( Number(values.ptyValue) === 3){ // 눈
-    //             todayWeatherLogo.classList.add('weather-icon--밤--눈--맑음');
-    //         }else if( Number(values.ptyValue) === 4){ //소나기
-    //             todayWeatherLogo.classList.add('weather-icon--밤--소나기--맑음');
-    //         }
-    //     }else{ // 낮
-    //         if( Number(values.ptyValue) === 0 ){
-    //             todayWeatherLogo.classList.add('weather-icon--낮--맑음');
-    //         }else if( Number(values.ptyValue) === 1){ // 비
-    //             todayWeatherLogo.classList.add('weather-icon--낮--비--맑음');
-    //         }else if( Number(values.ptyValue) === 2){ //눈/비
-    //             todayWeatherLogo.classList.add('weather-icon--낮--눈비--맑음');
-    //         }else if( Number(values.ptyValue) === 3){ // 눈
-    //             todayWeatherLogo.classList.add('weather-icon--낮--눈--맑음');
-    //         }else if( Number(values.ptyValue) === 4){ //소나기
-    //             todayWeatherLogo.classList.add('weather-icon--낮--소나기--맑음');
-    //         }
-    //     }
-
-    // }else if(Number(values.skyValue) === 3){
-    //     todayWeatherSecondSpan.textContent = '구름 많음';
-    //     if(  19 < nowFcstTime.slice(0,-2) ) { // 20시 부터 저녁 icon을 보여주기 위함
-    //         if( Number(values.ptyValue) === 0 ){
-    //             todayWeatherLogo.classList.add('weather-icon--밤--구름많음');
-    //         }else if( Number(values.ptyValue) === 1){ // 비
-    //             todayWeatherLogo.classList.add('weather-icon--밤--비--구름많음');
-    //         }else if( Number(values.ptyValue) === 2){ //눈/비
-    //             todayWeatherLogo.classList.add('weather-icon--밤--눈비--구름많음');
-    //         }else if( Number(values.ptyValue) === 3){ // 눈
-    //             todayWeatherLogo.classList.add('weather-icon--밤--눈--구름많음');
-    //         }else if( Number(values.ptyValue) === 4){ //소나기
-    //             todayWeatherLogo.classList.add('weather-icon--밤--소나기--구름많음');
-    //         }
-    //     }else{ // 낮
-    //         if( Number(values.ptyValue) === 0 ){
-    //             todayWeatherLogo.classList.add('weather-icon--낮--구름많음');
-    //         }else if( Number(values.ptyValue) === 1){ // 비
-    //             todayWeatherLogo.classList.add('weather-icon--낮--비--구름많음');
-    //         }else if( Number(values.ptyValue) === 2){ //눈/비
-    //             todayWeatherLogo.classList.add('weather-icon--낮--눈비--구름많음');
-    //         }else if( Number(values.ptyValue) === 3){ // 눈
-    //             todayWeatherLogo.classList.add('weather-icon--낮--눈--구름많음');
-    //         }else if( Number(values.ptyValue) === 4){ //소나기
-    //             todayWeatherLogo.classList.add('weather-icon--낮--소나기--구름많음');
-    //         }
-    //     }
-    // }else if( Number(values.skyValue) === 4){
-    //     todayWeatherSecondSpan.textContent = '흐림';
-    //     if(  19 < nowFcstTime.slice(0,-2) ) { // 20시 부터 저녁 icon을 보여주기 위함
-    //         if( Number(values.ptyValue) === 0 ){
-    //             todayWeatherLogo.classList.add('weather-icon--밤--흐림');
-    //         }else if( Number(values.ptyValue) === 1){ // 비
-    //             todayWeatherLogo.classList.add('weather-icon--밤--비--흐림');
-    //         }else if( Number(values.ptyValue) === 2){ //눈/비
-    //             todayWeatherLogo.classList.add('weather-icon--밤--눈비--흐림');
-    //         }else if( Number(values.ptyValue) === 3){ // 눈
-    //             todayWeatherLogo.classList.add('weather-icon--밤--눈--흐림');
-    //         }else if( Number(values.ptyValue) === 4){ //소나기
-    //             todayWeatherLogo.classList.add('weather-icon--밤--소나기--흐림');
-    //         }
-    //     }else{ // 낮
-    //         if( Number(values.ptyValue) === 0 ){
-    //             todayWeatherLogo.classList.add('weather-icon--낮--흐림');
-    //         }else if( Number(values.ptyValue) === 1){ // 비
-    //             todayWeatherLogo.classList.add('weather-icon--낮--비--흐림');
-    //         }else if( Number(values.ptyValue) === 2){ //눈/비
-    //             todayWeatherLogo.classList.add('weather-icon--낮--눈비--흐림');
-    //         }else if( Number(values.ptyValue) === 3){ // 눈
-    //             todayWeatherLogo.classList.add('weather-icon--낮--눈--흐림');
-    //         }else if( Number(values.ptyValue) === 4){ //소나기
-    //             todayWeatherLogo.classList.add('weather-icon--낮--소나기--흐림');
-    //         }
-    //     }
-    // }
     todayWeatherFirstSpan.textContent =  `${values.tmpValue}°`;
 
 
@@ -302,23 +207,23 @@ const getWeatherList = (weatherList) =>{
     getGraphBar(nextWeather1.tmpValue,nextWeather2.tmpValue,nextWeather3.tmpValue,nextWeather4.tmpValue,nextWeather5.tmpValue);
 
     nextDayWeatherTemp1.textContent = `${nextWeather1.tmpValue}°`;
-    nextDayWeatherImage1.textContent = ``;
+    // nextDayWeatherImage1.textContent = ``;
     nextDayWeatherHours1.textContent = `${delZeroHours_1}시`;
 
     nextDayWeatherTemp2.textContent = `${nextWeather2.tmpValue}°`;
-    nextDayWeatherImage2.textContent = ``;
+    // nextDayWeatherImage2.textContent = ``;
     nextDayWeatherHours2.textContent = delZeroHours_2;
 
     nextDayWeatherTemp3.textContent = `${nextWeather3.tmpValue}°`;
-    nextDayWeatherImage3.textContent = ``;
+    // nextDayWeatherImage3.textContent = ``;
     nextDayWeatherHours3.textContent = delZeroHours_3;
 
     nextDayWeatherTemp4.textContent = `${nextWeather4.tmpValue}°`;
-    nextDayWeatherImage4.textContent = ``;
+    // nextDayWeatherImage4.textContent = ``;
     nextDayWeatherHours4.textContent = delZeroHours_4;
 
     nextDayWeatherTemp5.textContent = `${nextWeather5.tmpValue}°`;
-    nextDayWeatherImage5.textContent = ``;
+    // nextDayWeatherImage5.textContent = ``;
     nextDayWeatherHours5.textContent = delZeroHours_5;
 }
 
@@ -330,7 +235,6 @@ getWeatherApi(getWeatherList);
 // 하늘상태 (SKY) 코드 : 맑음(1), 구름많음(3), 흐림(4)
 
 
-
 const addClassListWeatherIcon = (fcstTime, values, className) =>{
     // console.log( time, values, className);
 
@@ -339,7 +243,7 @@ const addClassListWeatherIcon = (fcstTime, values, className) =>{
             todayWeatherSecondSpan.textContent = '맑음';
         }
 
-        if(  19 < fcstTime.slice(0,-2) ) { // 20시 부터 저녁 icon을 보여주기 위함
+        if(  19 < fcstTime.slice(0,-2) || fcstTime.slice(0,-2) < 5 ) { // 20시 부터 저녁 icon을 보여주기 위함
             if( Number(values.ptyValue) === 0 ){
                 className.classList.add('weather-icon--밤--맑음');
             }else if( Number(values.ptyValue) === 1){ // 비
@@ -370,7 +274,8 @@ const addClassListWeatherIcon = (fcstTime, values, className) =>{
             todayWeatherSecondSpan.textContent = '구름 많음';
         }
 
-        if(  19 < fcstTime.slice(0,-2) ) { // 20시 부터 저녁 icon을 보여주기 위함
+        if(  19 < fcstTime.slice(0,-2) || fcstTime.slice(0,-2) < 5) { // 20시 부터 저녁 icon을 보여주기 위함
+            console.log('뭐야 몇시인데..?',fcstTime.slice(0,-2) < 5 );
             if( Number(values.ptyValue) === 0 ){
                 className.classList.add('weather-icon--밤--구름많음');
             }else if( Number(values.ptyValue) === 1){ // 비
@@ -400,7 +305,7 @@ const addClassListWeatherIcon = (fcstTime, values, className) =>{
             todayWeatherSecondSpan.textContent = '흐림';
         }
        
-        if(  19 < fcstTime.slice(0,-2) ) { // 20시 부터 저녁 icon을 보여주기 위함
+        if(  19 < fcstTime.slice(0,-2) || fcstTime.slice(0,-2) < 5) { // 20시 부터 저녁 icon을 보여주기 위함
             if( Number(values.ptyValue) === 0 ){
                 className.classList.add('weather-icon--밤--흐림');
             }else if( Number(values.ptyValue) === 1){ // 비
@@ -439,10 +344,9 @@ const addClassListWeatherIcon = (fcstTime, values, className) =>{
 
 
 const getGraphBar = (tmp1,tmp2,tmp3, tmp4, tmp5)=>{
-//tmpValue값만 쓸꺼임
+    //tmpValue값만 쓸꺼임
     const temperArray = new Array(tmp1, tmp2, tmp3, tmp4, tmp5);
-    // console.log(tmp1,tmp2,tmp3,tmp4, tmp5 )
-    console.log(temperArray);
+    
     
     const minHeight = 7;
     const maxHeight = 43;
@@ -469,10 +373,9 @@ const getGraphBar = (tmp1,tmp2,tmp3, tmp4, tmp5)=>{
         const bar = document.querySelector(`.next--weather--${index+1} .bar`);
         bar.style.height= `${barHeight}px`;
 
-        console.log('몇번째? => ',index, '온도는? =>',temp, '바의 길이는?! = >',barHeight ,'offsetTop은? =>', bar.offsetTop);
         // let y = bar.offsetTop ;
         let y = 47 - barHeight + 37;
-// 132(부모 요소) - lineHeight(bar길이 ) -35 
+    // 132(부모 요소) - lineHeight(bar길이 ) -35 
         if( index === 0){
            
             if(index === 0 ){
@@ -492,41 +395,7 @@ const getGraphBar = (tmp1,tmp2,tmp3, tmp4, tmp5)=>{
         }
         
     });
-
-    
-    // barList.push( x , x );
     drawGraph(barList);
-    console.log(barList);
-
-
-
-    /*
-   
-        네이버 에서는  height 값을
-        34도  31도    28도   25도   25도
-         43px  31       19     7      7 
-    
-
-         27도 26도 28도 30도 31도
-        14px 7px  21px  35px  43px 
-
-
-         // 기온을 기준으로 막대 높이 정규화
-        const normalizedHeight = (entry.temp - minTemp) / (maxTemp - minTemp) * maxBarHeight;
-        barSpan.style.height = `${normalizedHeight}px`;
-
-        34도 일떼 -> (34-25) / (31 - 25) * 100  = 9 / 6 *100  = 150px...?
-        
-
-        꺽은선 그래프 
-        -> 32px+ barHeight 값  ==> 이게 그려야할 y축이 될 것!!! (32px은 이미지 / 이미지 크기만큼 빼줘야..?)
-        x축은 0부터 시작..!?! 0 -> 15까지 찍고, 다음 점은 16+15= 31 만큼그리고, 
-        마지막은 y값은 그대로, x값만 + 15 해서 그려주기
-
-    
-    */
-
-
 }
 
 const canvas = document.getElementById('lineChart');
